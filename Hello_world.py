43af8e473c383c1b58dc464bc14075aa20db379b
Louis VERHEGGE <lverhegge@outlook.fr>
2025-03-19 18:05:32 +0100

    Mohamed

--- Hello_world.py
+++ Hello_world.py
@@ -7,6 +7,6 @@ print("Hello world")
 
 # We introduce some interaction with the user
 
-name = input("What's your name?")
-print("\nHello " + name)
+name = input("Mais qui est donc le voleur ?")
+print("\nC'est bien évidemment " + name)
 
